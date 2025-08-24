3261062a4e0c381b5b01f1f2ba0ff63e68e5b9f3
donot <donnutsaysnothing@gmail.com>
2025-08-24 15:45:14 -0300

    Add files via upload

---
+++ .config/fish/config.fish
@@ -0,0 +1,26 @@
+set -g fish_greeting
+
+# List Directory
+alias l='eza -lh  --icons=auto' # long list
+alias ls='eza -1   --icons=auto' # short list
+alias ll='eza -lha --icons=auto --sort=name --group-directories-first' # long list all
+alias ld='eza -lhD --icons=auto' # long list dirs
+alias lt='eza --icons=auto --tree' # list folder as tree
+
+# Handy change dir shortcuts
+abbr .. 'cd ..'
+abbr ... 'cd ../..'
+abbr .3 'cd ../../..'
+abbr .4 'cd ../../../..'
+abbr .5 'cd ../../../../..'
+
+# Always mkdir a path (this doesn't inhibit functionality to make a single dir)
+abbr mkdir 'mkdir -p'
+
+# Created by `pipx` on 2025-06-30 21:05:27
+set PATH $PATH ~/.local/bin
+
+function ls
+    command ls --color=auto $argv
+end
+
---
+++ .config/rofi/launchers/gamelauncher/gamelauncher.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+rofi -modi games -show games -theme ~/.config/rofi/launchers/gamelauncher/style-main.rasi
